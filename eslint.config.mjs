cef368c53a7b76a74c7a591e88db2c59c33b1313
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-04-04 08:44:03 +0900

    eslimtエラー修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off" // anyの使用を許可
+      "@typescript-eslint/no-explicit-any": "off", // anyの使用を許可
+      '@typescript-eslint/no-empty-object-type': 'off',
     }
   }
 ];
